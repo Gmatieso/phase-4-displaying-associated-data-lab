4323863ae152d16d04e7180fe4a839fe50290ba9
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-12-04 14:16:22 +0300

    Include items in Show method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def show
+    users = User.find(params[:id])
+    render json: users, include: :items
+    #using include item will call the .item method that is provided has_many and will serialize the item as a nested array of JSON data.
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "User  not found" }, status: :not_found
+  end
 end
